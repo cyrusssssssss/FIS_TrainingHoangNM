74b00ae8f1d13aa26f5d4d111e3bd493a67b2c19
nguyen hoang <nguyenhoang229201@gmail.com>
2022-06-08 22:01:59 +0700

    miniEx1,2

---
+++ Topic02/fresher-excel-maven/src/main/java/apache/poi/tryit/Transaction.java
@@ -0,0 +1,72 @@
+package apache.poi.tryit;
+
+
+public class Transaction {
+    private String type;
+    private String bankAcount;
+    private double amount;
+    private String message;
+    private String dateTime;
+
+    public Transaction() {
+    }
+
+    public Transaction(String type, String bankAcount, double amount, String message, String dateTime) {
+        this.type = type;
+        this.bankAcount = bankAcount;
+        this.amount = amount;
+        this.message = message;
+        this.dateTime = dateTime;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBankAcount() {
+        return bankAcount;
+    }
+
+    public void setBankAcount(String bankAcount) {
+        this.bankAcount = bankAcount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "type='" + type + '\'' +
+                ", bankAcount='" + bankAcount + '\'' +
+                ", amount=" + amount +
+                ", message='" + message + '\'' +
+                ", dateTime=" + dateTime +
+                '}';
+    }
+}
